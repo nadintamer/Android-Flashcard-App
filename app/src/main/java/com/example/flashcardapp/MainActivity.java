bf2856147cfc42c6e2791707e87029e66d53ebe8
Nadin Tamer <nadin.tamer@gmail.com>
2021-02-20 11:53:08 -0800

    Add multiple answer choices; show correctness; toggle visibility of answer choices

--- app/src/main/java/com/example/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    boolean answerChoicesVisible = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +35,60 @@ public class MainActivity extends AppCompatActivity {
                 flashcardQuestion.setVisibility(View.VISIBLE);
             }
         });
+
+        // User can tap on multiple choice answers to see whether they're correct
+        TextView incorrectAnswer1 = ((TextView) findViewById(R.id.flashcard_answer1));
+        TextView incorrectAnswer2 = ((TextView) findViewById(R.id.flashcard_answer3));
+        TextView correctAnswer = ((TextView) findViewById(R.id.flashcard_answer2));
+
+        incorrectAnswer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                incorrectAnswer1.setBackgroundColor(getResources().getColor(R.color.tomato, null));
+                incorrectAnswer2.setBackgroundColor(getResources().getColor(R.color.orange, null));
+                correctAnswer.setBackgroundColor(getResources().getColor(R.color.orange, null));
+            }
+        });
+
+        incorrectAnswer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                incorrectAnswer2.setBackgroundColor(getResources().getColor(R.color.tomato, null));
+                incorrectAnswer1.setBackgroundColor(getResources().getColor(R.color.orange, null));
+                correctAnswer.setBackgroundColor(getResources().getColor(R.color.orange, null));
+            }
+        });
+
+        correctAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                correctAnswer.setBackgroundColor(getResources().getColor(R.color.green, null));
+                incorrectAnswer1.setBackgroundColor(getResources().getColor(R.color.orange, null));
+                incorrectAnswer2.setBackgroundColor(getResources().getColor(R.color.orange, null));
+            }
+        });
+
+        ImageView toggleButton = ((ImageView) findViewById(R.id.toggle_choices_visibility));
+
+        // User can tap on toggle button to show/hide answer choices
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (answerChoicesVisible) {
+                    correctAnswer.setVisibility(View.INVISIBLE);
+                    incorrectAnswer1.setVisibility(View.INVISIBLE);
+                    incorrectAnswer2.setVisibility(View.INVISIBLE);
+                    toggleButton.setImageResource(R.drawable.eye_visible);
+                    answerChoicesVisible = false;
+                } else {
+                    correctAnswer.setVisibility(View.VISIBLE);
+                    incorrectAnswer1.setVisibility(View.VISIBLE);
+                    incorrectAnswer2.setVisibility(View.VISIBLE);
+                    toggleButton.setImageResource(R.drawable.eye_hidden);
+                    answerChoicesVisible = true;
+                }
+            }
+        });
+
     }
 }
