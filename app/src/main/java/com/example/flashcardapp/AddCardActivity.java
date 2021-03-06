10dcbcc52c560a0568efb5e5d0b1fb7b6908a842
Nadin Tamer <nadin.tamer@gmail.com>
2021-03-06 11:37:46 -0800

    added error message in creating question

--- app/src/main/java/com/example/flashcardapp/AddCardActivity.java
+++ app/src/main/java/com/example/flashcardapp/AddCardActivity.java
@@ -9,6 +9,8 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class AddCardActivity  extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +32,21 @@ public class AddCardActivity  extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent data = new Intent();
-                data.putExtra("question", questionField.getText().toString());
-                data.putExtra("answer", answerField.getText().toString());
-                setResult(RESULT_OK, data);
-                finish();
+                String enteredQuestion = questionField.getText().toString();
+                String enteredAnswer = answerField.getText().toString();
+                
+                if (enteredQuestion.isEmpty() || enteredAnswer.isEmpty()) {
+                    Snackbar.make(saveButton,
+                            "Please enter both the question and answer!",
+                            Snackbar.LENGTH_SHORT)
+                            .show();
+                } else {
+                    Intent data = new Intent();
+                    data.putExtra("question", enteredQuestion);
+                    data.putExtra("answer", enteredAnswer);
+                    setResult(RESULT_OK, data);
+                    finish();
+                }
             }
         });
     }
--- app/src/main/java/com/example/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
     boolean answerChoicesVisible = true;
 
